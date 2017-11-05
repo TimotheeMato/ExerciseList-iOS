25023cd35a57cf499c1a30adfc4ec5e282978711
Timothee MATO <timothee.mato@etixgroup.com>
2017-11-05 19:41:12 +0100

    Initial commit

---
+++ ExerciseList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExerciseList
+//
+//  Created by Timothee MATO on 11/5/17.
+//  Copyright © 2017 TimotheeMato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
