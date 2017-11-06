d98b76d05358a07baa13e9b16b2bc0076355c5fd
Timothee MATO <timothee.mato@etixgroup.com>
2017-11-06 09:07:01 +0100

    Clean some code

--- ExerciseList/Utils/DataHandler.swift
+++ ExerciseList/Utils/DataHandler.swift
@@ -15,40 +15,40 @@ class DataHandler {
     var databaseService: DatabaseService!
     
     public func getAllExercises(completion: @escaping ([Exercise]) -> Void, failure: @escaping () -> Void) {
+        let exerciseListDb = [Exercise]()
         do {
             databaseService = try DatabaseService()
-            let exerciseListDb = try databaseService.getAllExercises()
-            if exerciseListDb.count != 0 {
-                completion(exerciseListDb)
-            } else {
-                provider.request(.getExercises) { result in
-                    switch result {
-                    case let .success(moyaResponse):
-                        let data = moyaResponse.data
-                        let json = JSON(data: data)
-                        if json["data"].array != nil {
-                            var exerciseList = [Exercise]()
-                            for (_, subJson):(String, JSON) in json["data"] {
-                                let exercise = Exercise(json: subJson)
-                                exerciseList.append(exercise)
-                                do {
-                                    try self.databaseService.storeExercise(exercise: exercise)
-                                } catch {
-                                    print("Error info: \(error)")
-                                }
-                            }
-                            completion(exerciseList)
-                        } else {
-                            failure()
-                        }
-                    case let .failure(_):
-                        failure()
-                    }
-                }
-            }
+            exerciseListDb = try databaseService.getAllExercises()
         } catch {
             print("Error info: \(error)")
-            failure()
+        }
+        if exerciseListDb.count != 0 {
+            completion(exerciseListDb)
+        } else {
+            provider.request(.getExercises) { result in
+                switch result {
+                case let .success(moyaResponse):
+                    let data = moyaResponse.data
+                    let json = JSON(data: data)
+                    if json["data"].array != nil {
+                        var exerciseList = [Exercise]()
+                        for (_, subJson):(String, JSON) in json["data"] {
+                            let exercise = Exercise(json: subJson)
+                            exerciseList.append(exercise)
+                            do {
+                                try self.databaseService.storeExercise(exercise: exercise)
+                            } catch {
+                                print("Error info: \(error)")
+                            }
+                        }
+                        completion(exerciseList)
+                    } else {
+                        failure()
+                    }
+                case let .failure(_):
+                    failure()
+                }
+            }
         }
     }
 }
